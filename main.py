c09e493380b6ca200a6d02226a20bf6fa12eb68f
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-07-18 10:28:13 +0300

    adding light_bulb_replacement function

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ def find_index_of_darkest_street_light(road_length: int, not_working_street_ligh
     return darkest_light_index
 
 
+def find_minimal_bulbs_replaced(road_length: int, not_working_street_lights: list[int]) -> int:
+    bulbs_replaced = 0
+
+    for i in range(road_length // 20 + 1):
+        if i not in not_working_street_lights:
+            distance = i * 20
+            illumination = 3 ** (-(distance / 100) ** 2)
+
+            if illumination < 1:
+                bulbs_replaced += 1
+
+    return bulbs_replaced
+
+
 road_length = 200
 not_working_street_lights = [4, 5, 6]
 print(find_index_of_darkest_street_light(road_length, not_working_street_lights))
+print(find_minimal_bulbs_replaced(road_length, not_working_street_lights))
